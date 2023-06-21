4e992b9e2a774283c7d965e03ddbfa293a282cc9
tk10589 <k1taka2014@gmail.com>
2023-06-21 17:01:04 +0900

    ユーザ登録及びログイン機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9])/ }
+
+end
